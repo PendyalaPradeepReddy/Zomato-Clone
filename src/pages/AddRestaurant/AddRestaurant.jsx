976a5cc131c3d79aa6e2e2f9fffdc4555433e5a5
unknown <pradipreddy1605@gmail.com>
2025-03-09 16:20:05 +0530

    Initial commit

---
+++ src/pages/AddRestaurant/AddRestaurant.jsx
@@ -0,0 +1,26 @@
+import AddRestaurantHeader from '../../components/AddRestaurantComponents/AddRestaurantHeader/AddRestaurantHeader';
+import WhyShouldYouPWZ from '../../components/AddRestaurantComponents/WhyShouldYouPWZ/WhyShouldYouPWZ';
+import HowItWorks from '../../components/AddRestaurantComponents/HowItWorks/HowItWorks';
+import SearchListedRestaurant from '../../components/AddRestaurantComponents/SearchListedRestaurant/SearchListedRestaurant';
+import SmallBannerCard from '../../utils/Cards/SmallBannerCard/SmallBannerCard';
+import FrequentlyAskedQues from '../../components/HomeComponents/FrequentlyAskedQues/FrequentlyAskedQues';
+import AddRestaurantSec from '../../components/AddRestaurantComponents/AddRestaurantSec/AddRestaurantSec';
+import Footer from '../../components/Footer/Footer';
+import React from 'react'
+
+import css from './AddRestaurant.module.css'
+
+let AddRestaurant = () => {
+    return <div>
+        <AddRestaurantHeader />
+        <WhyShouldYouPWZ />
+        <HowItWorks />
+        <SearchListedRestaurant />
+        <SmallBannerCard />
+        <FrequentlyAskedQues />
+        <AddRestaurantSec />
+        <Footer />
+    </div>
+}
+
+export default AddRestaurant;
